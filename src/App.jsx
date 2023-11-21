4f822cde46b9db30de7c89f93b774a75f3389bbd
yogesh dubey <yogesh.dubey.0804@gmail.com>
2023-11-21 23:33:21 +0530

    added privacy

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Toaster } from "react-hot-toast";
 import {
   createBrowserRouter,
   redirect,
@@ -5,18 +6,19 @@ import {
 } from "react-router-dom";
 import NotFound from "./components/404/not_found_component";
 import { AuthProvider } from "./contexts/auth";
-import Login from "./pages/Login";
+import Cart from "./pages/Cart/Cart";
 import Events from "./pages/Events/Events";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 import UpdateUser from "./pages/UpdateUser/UpdateUser";
 import PrivateRoute from "./routes/private_route";
-import Home from "./pages/Home";
-import Cart from "./pages/Cart/Cart";
-import { Toaster } from "react-hot-toast";
 
 
-import logout from "./utils/logout";
 import { CartProvider } from "./contexts/cart";
 import PaymentPage from "./pages/Payment/PaymentPage";
+import PrivacyPage from "./pages/privacy/PrivacyPage";
+import logout from "./utils/logout";
+
 
 const router = createBrowserRouter([
   {
@@ -48,6 +50,10 @@ const router = createBrowserRouter([
   {
     path: "/login",
     element: <Login />,
+  },
+  {
+    path: "/privacy",
+    element: <PrivacyPage />,
   },
   {
     path: "/logout",
